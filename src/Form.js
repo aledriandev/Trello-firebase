840095c692121c377be8692729b5b20ef6d08bb2
unknown <alejandraadriantejda@gmail.com>
2017-11-21 07:48:56 -0500

    adicionar clase

--- src/Form.js
+++ src/Form.js
@@ -9,7 +9,7 @@ class  Form   extends Component {
             <form onSubmit={(e) => {
                 e.preventDefault();
                 add(this.inputRef.value, userActual, iTeam, iBoard, iList);
-                console.log('input',this.inputRef.value)
+                this.inputRef.value = ''; 
             }}>
                 <input placeholder={placeholder} type="text" ref={e => this.inputRef = e} />
                 <button className='list-add'><h4>Add</h4></button>
